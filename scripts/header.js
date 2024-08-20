adeccc09d2ea4ab9eba69f5c28c9bb9558c533fd
Thyago Ribeiro <thyagoribeiro7226@gmail.com>
2024-08-20 18:55:28 -0300

    feat: Added a new header.js file for the header scroll effect

--- scripts/header.js
+++ scripts/header.js
@@ -3,9 +3,9 @@ const headerQuery = document.querySelector('.nav_bar');
 window.addEventListener('scroll', () => {        
     let yAxis = window.scrollY;
 
-    // console.log(yAxis); logs out the Y scroll.
-    
-    // If the yAxis if greater or equal to 200px, add the class 'active' in the DOM tree, else remove it.
+    // console.log(yAxis); logs out the Y scroll position
+
+    // If the yAxis if greater or equal to 200px, add the class 'active' in the DOM tree, else remove it
     if (yAxis >= 200){
         headerQuery.classList.add('active');
     }

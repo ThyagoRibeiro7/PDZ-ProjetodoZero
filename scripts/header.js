58ec41d10546868fac0edbf96d9b0e9e1b0f7d77
Thyago Ribeiro <thyagoribeiro7226@gmail.com>
2024-08-22 17:32:46 -0300

    refactor: changed the if statement syntax to the ternary operator

--- scripts/header.js
+++ scripts/header.js
@@ -3,15 +3,7 @@ const headerQuery = document.querySelector('.nav_bar');
 window.addEventListener('scroll', () => {        
     let yAxis = window.scrollY;
 
-    // console.log(yAxis); logs out the Y scroll position
-
-    // If the yAxis if greater or equal to 200px, add the class 'active' in the DOM tree, else remove it
-    if (yAxis >= 200){
-        headerQuery.classList.add('active');
-    }
-    else{
-        headerQuery.classList.remove('active');
-    };
+    yAxis >= 200 ? headerQuery.classList.add('active') : headerQuery.classList.remove('active');
 
 });
 

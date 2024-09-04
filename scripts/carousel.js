3e66017d4a3e3d8f84aa5464819060f4879c8d52
Thyago Ribeiro <thyagoribeiro7226@gmail.com>
2024-09-04 20:53:14 -0300

    update: deleted the function that make the carousel arrows dissapear

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -8,28 +8,13 @@ function carousel() {
     mainBtns.forEach(btn => {
 
         btn.addEventListener('click', () => {
-            firstWrapper.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
-            showAndHideMainBtns();
+            firstWrapper.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;    
         });
 
         destaquesBtns.forEach(btn => {
             btn.addEventListener('click', () => {
                 secondWrapper.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
-                showAndHideDestaquesBtns();
             });
         });
     });
 };
-
-function showAndHideMainBtns() {
-    let firstBtn = mainBtns[0];
-    firstWrapper.scrollLeft == 0 ? firstBtn.style.display = 'none' : firstBtn.style.display = 'flex';
-};
-
-function showAndHideDestaquesBtns() {
-    let firstMainBtn = mainBtns[0];
-
-    secondWrapper.scrollLeft == 0 ? firstMainBtn.style.display = 'none' : firstMainBtn.style.display = 'flex';
-}
-
-carousel();
